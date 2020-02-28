acad2a09ec8aec657aad8d19dad822da40a55f3c
John Ivanov <johnivo@mail.ru>
2020-02-28 13:05:10 +0300

    1.2. Лямбды и шаблон wrapper.[#187170]

--- src/main/java/ru/job4j/hiber/persistence/DBStore.java
+++ src/main/java/ru/job4j/hiber/persistence/DBStore.java
@@ -8,6 +8,7 @@ import org.hibernate.cfg.Configuration;
 import ru.job4j.hiber.models.Item;
 
 import java.util.List;
+import java.util.function.Function;
 
 /**
  * @author John Ivanov (johnivo@mail.ru)
@@ -29,87 +30,148 @@ public class DBStore implements IStore {
         return INSTANCE;
     }
 
-    @Override
-    public Item add(Item item) {
+    private <T> T tx(final Function<Session, T> command) {
         Transaction tx = null;
+        T rsl = null;
         try (Session session = factory.openSession()) {
             tx = session.beginTransaction();
-            session.save(item);
+            rsl = command.apply(session);
             tx.commit();
+            return rsl;
         } catch (HibernateException e) {
             if (tx != null) {
                 tx.rollback();
             }
             e.printStackTrace();
         }
-        return item;
+        return rsl;
+    }
+
+    @Override
+    public Item add(Item item) {
+        return this.tx(
+                session -> {
+                    session.save(item);
+                    return item;
+                }
+        );
     }
 
     @Override
     public Item update(Item item) {
-        Transaction tx = null;
-        try (Session session = factory.openSession()) {
-            tx = session.beginTransaction();
-            session.update(item);
-            tx.commit();
-        } catch (HibernateException e) {
-            if (tx != null) {
-                tx.rollback();
-            }
-            e.printStackTrace();
-        }
-        return item;
+        return this.tx(
+                session -> {
+                    session.update(item);
+                    return item;
+                }
+        );
     }
 
     @Override
     public void delete(Item item) {
-        Transaction tx = null;
-        try (Session session = factory.openSession()) {
-            tx = session.beginTransaction();
-            session.delete(item);
-            tx.commit();
-        } catch (HibernateException e) {
-            if (tx != null) {
-                tx.rollback();
-            }
-            e.printStackTrace();
-        }
+        this.tx(
+                session -> {
+                    session.delete(item);
+                    return null;
+                }
+        );
     }
 
     @Override
     public Item get(Item item) {
-        Transaction tx = null;
-        Item result = null;
-        try (Session session = factory.openSession()) {
-            tx = session.beginTransaction();
-            result = session.get(Item.class, item.getId());
-            tx.commit();
-        } catch (HibernateException e) {
-            if (tx != null) {
-                tx.rollback();
-            }
-            e.printStackTrace();
-        }
-        return result;
+        return this.tx(
+                session -> session.get(Item.class, item.getId())
+        );
     }
 
     @Override
     public List<Item> getAll() {
-        Transaction tx = null;
-        List<Item> result = null;
-        try (Session session = factory.openSession()) {
-            tx = session.beginTransaction();
-            //result = session.createQuery("from Item", Item.class).list();
-            //result = session.createQuery("from ru.job4j.hiber.models.Item").list();
-            result = session.createQuery("FROM Item AS i ORDER BY i.id ASC").list();
-            tx.commit();
-        } catch (HibernateException e) {
-            if (tx != null) {
-                tx.rollback();
-            }
-            e.printStackTrace();
-        }
-        return result;
+        return this.tx(
+                session -> session.createQuery("FROM Item AS i ORDER BY i.id ASC").list()
+        );
     }
+
+//    @Override
+//    public Item add(Item item) {
+//        Transaction tx = null;
+//        try (Session session = factory.openSession()) {
+//            tx = session.beginTransaction();
+//            session.save(item);
+//            tx.commit();
+//        } catch (HibernateException e) {
+//            if (tx != null) {
+//                tx.rollback();
+//            }
+//            e.printStackTrace();
+//        }
+//        return item;
+//    }
+//
+//    @Override
+//    public Item update(Item item) {
+//        Transaction tx = null;
+//        try (Session session = factory.openSession()) {
+//            tx = session.beginTransaction();
+//            session.update(item);
+//            tx.commit();
+//        } catch (HibernateException e) {
+//            if (tx != null) {
+//                tx.rollback();
+//            }
+//            e.printStackTrace();
+//        }
+//        return item;
+//    }
+//
+//    @Override
+//    public void delete(Item item) {
+//        Transaction tx = null;
+//        try (Session session = factory.openSession()) {
+//            tx = session.beginTransaction();
+//            session.delete(item);
+//            tx.commit();
+//        } catch (HibernateException e) {
+//            if (tx != null) {
+//                tx.rollback();
+//            }
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    @Override
+//    public Item get(Item item) {
+//        Transaction tx = null;
+//        Item result = null;
+//        try (Session session = factory.openSession()) {
+//            tx = session.beginTransaction();
+//            result = session.get(Item.class, item.getId());
+//            tx.commit();
+//        } catch (HibernateException e) {
+//            if (tx != null) {
+//                tx.rollback();
+//            }
+//            e.printStackTrace();
+//        }
+//        return result;
+//    }
+//
+//    @Override
+//    public List<Item> getAll() {
+//        Transaction tx = null;
+//        List<Item> result = null;
+//        try (Session session = factory.openSession()) {
+//            tx = session.beginTransaction();
+//            //result = session.createQuery("from Item", Item.class).list();
+//            //result = session.createQuery("from ru.job4j.hiber.models.Item").list();
+//            result = session.createQuery("FROM Item AS i ORDER BY i.id ASC").list();
+//            tx.commit();
+//        } catch (HibernateException e) {
+//            if (tx != null) {
+//                tx.rollback();
+//            }
+//            e.printStackTrace();
+//        }
+//        return result;
+//    }
 
 }
